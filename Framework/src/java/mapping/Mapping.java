e04cb1fc358e6ffd6a201651b9fbc05483e28dec
Kanto Andrianotahina <118660424+Kanto-Andrianotahina@users.noreply.github.com>
2023-03-07 10:00:42 +0300

    Add files via upload

---
+++ Framework/src/java/mapping/Mapping.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapping;
+
+/**
+ *
+ * @author Kanto
+ */
+public class Mapping {
+    String className;
+    String method;
+
+    public String getClassName() {return className;}
+    public void setClassName(String className) {this.className = className;}
+
+    public String getMethod() {return method;}
+    public void setMethod(String method) {this.method = method;}
+    
+    
+    
+   
+    
+}
